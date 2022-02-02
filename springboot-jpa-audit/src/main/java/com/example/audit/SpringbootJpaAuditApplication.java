51ef2882d0b06079d2eb6a0da4db6ebbe485b6fa
Harikrishnan P <hariktd1@gmail.com>
2022-02-02 21:53:26 +0100

    456

--- springboot-jpa-audit/src/main/java/com/example/audit/SpringbootJpaAuditApplication.java
+++ springboot-jpa-audit/src/main/java/com/example/audit/SpringbootJpaAuditApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class SpringbootJpaAuditApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringbootJpaAuditApplication.class, args);
 
 
